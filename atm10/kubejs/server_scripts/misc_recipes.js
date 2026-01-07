8c3c6b525b1d38da0f75fe8c939fc1e0b5bfb72c
Atlas V <atlasvpersonal@gmail.com>
2026-01-07 12:11:26 +0000

    fix: Corrected registry name for twilight forest wrought iron bar in `misc_recipes.js`

--- atm10/kubejs/server_scripts/misc_recipes.js
+++ atm10/kubejs/server_scripts/misc_recipes.js
@@ -4,8 +4,8 @@
 ServerEvents.recipes(event => {
     // Twilight Forest
     // Wrought Iron
-    event.smelting('2x twilightforest:wrought_iron', 'minecraft:iron_ingot')
-    event.blasting('2x twilightforest:wrought_iron', 'minecraft:iron_ingot')
+    event.smelting('2x twilightforest:wrought_iron_bar', 'minecraft:iron_ingot')
+    event.blasting('2x twilightforest:wrought_iron_bar', 'minecraft:iron_ingot')
     // Coronation Carpet
     event.shapeless(Item.of('twilightforest:coronation_carpet', 8),
         ['8x minecraft:red_carpet', 'minecraft:gold_nugget'])
